e7a66fc82f546f83cdee20c93481c43f569a15d6
junhaobearxiong <jxiong3@jhu.edu>
2019-04-10 16:04:58 -0400

    Cleanup

--- utils.py
+++ utils.py
@@ -90,84 +90,29 @@ def sort_graph(graph, inner_labels):
     return graph
 
 
-'''
-def permute_edges(A):
-    # return np.random.permutation(A)
-    return np.random.permutation(A.flatten()).reshape(A.shape)
+def pvalue(A, B, indept_test, transform_func, k=10, set_k=False, null_mc=500,
+           block_est_repeats=1):
+    test_stat_alternative, _ = indept_test.test_statistic(
+        matrix_X=transform_func(A), matrix_Y=transform_func(B))
 
+    block_assignment = estimate_block_assignment(A, B, k=k, set_k=set_k,
+                                                 num_repeats=block_est_repeats)
+    B_sorted = sort_graph(B, block_assignment)
 
-def permute_matrix(A):
-    permuted_indices = np.random.permutation(np.arange(A.shape[0]))
-    A = A[np.ix_(permuted_indices, permuted_indices)]
-    return A
-
-
-def block_permute(A, block_assignment):
-    permuted_A = np.zeros_like(A)
-    unique_blocks = np.unique(block_assignment)
-    for i in unique_blocks:
-        block_i_idx = np.where(block_assignment == i)[0]
-        for j in unique_blocks:
-            block_j_idx = np.where(block_assignment == j)[0]
-            block = A[np.ix_(block_i_idx, block_j_idx)]
-            if i == j:
-                permuted_block = permute_matrix(block)
-            else:
-                permuted_block = permute_edges(block)
-            permuted_A[np.ix_(block_i_idx, block_j_idx)] = permuted_block
-    permuted_A = symmetrize(permuted_A)
-    return permuted_A
-'''
-
-
-def diff_square(x): return 4 * np.square(x-0.5)
-
-
-def linear(x): return 2*x
-
-
-def square(x): return np.square(x)
-
-
-def exponential(x): return math.exp(x) / math.exp(1)
-
-
-def sine(x): return math.sin(2*x)
-
-
-def mild_diff_square(x): return np.square(x-0.5) + 0.5
-
-
-def identity(x): return x
-
-
-def power(indept_test, sample_func, transform_func, mc=500, alpha=0.05,
-          is_null=False, **kwargs):
-    test_stat_null_array = np.zeros(mc)
-    test_stat_alt_array = np.zeros(mc)
-    for i in range(mc):
-        A, B = sample_func(**kwargs)
-        if is_null:
-            A = permute_matrix(A)
-        test_stat_alt, _ = indept_test.test_statistic(
-            matrix_X=transform_func(A), matrix_Y=transform_func(B))
-        test_stat_alt_array[i] = test_stat_alt
-
-        # generate the null by permutation
-        A_null = permute_matrix(A)
+    test_stat_null_array = np.zeros(null_mc)
+    for j in tqdm(range(null_mc)):
+        # A_null is the permuted matrix after being sorted by block assignment
+        A_null = block_permute(A, block_assignment)
         test_stat_null, _ = indept_test.test_statistic(
-            matrix_X=transform_func(A_null), matrix_Y=transform_func(B))
-        test_stat_null_array[i] = test_stat_null
-    # if pearson, use the absolute value of test statistic then use one-sided
-    # rejection region
-    if indept_test.get_name() == 'pearson':
-        test_stat_null_array = np.absolute(test_stat_null_array)
-        test_stat_alt_array = np.absolute(test_stat_alt_array)
-    critical_value = np.sort(test_stat_null_array)[math.ceil((1-alpha)*mc)]
-    power = np.where(test_stat_alt_array > critical_value)[0].shape[0] / mc
-    return power
+            matrix_X=transform_func(A_null), matrix_Y=transform_func(B_sorted))
+        test_stat_null_array[j] = test_stat_null
+
+    p_value = np.where(test_stat_null_array > test_stat_alternative)[
+        0].shape[0] / test_stat_null_array.shape[0]
+    return p_value
 
 
+'''
 def pvalue(A, B, indept_test, transform_func, null_mc=1000):
     test_stat_alternative, _ = indept_test.test_statistic(
         matrix_X=transform_func(A), matrix_Y=transform_func(B))
@@ -182,6 +127,7 @@ def pvalue(A, B, indept_test, transform_func, null_mc=1000):
     p_value = np.where(test_stat_null_array > test_stat_alternative)[
         0].shape[0] / test_stat_null_array.shape[0]
     return p_value
+'''
 
 
 def pvalue_distribution(sample_func, indept_test, transform_func, null_mc=1000,
@@ -245,3 +191,24 @@ def sbm_params(setting='homog_balanced', a=0.7, b=0.5, c=0.3, k=2):
         L[np.triu_indices(k, 1)] = b
         L = symmetrize(L)
     return L
+
+
+def diff_square(x): return 4 * np.square(x-0.5)
+
+
+def linear(x): return 2*x
+
+
+def square(x): return np.square(x)
+
+
+def exponential(x): return math.exp(x) / math.exp(1)
+
+
+def sine(x): return math.sin(2*x)
+
+
+def mild_diff_square(x): return np.square(x-0.5) + 0.5
+
+
+def identity(x): return x
