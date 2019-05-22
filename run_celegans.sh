06608923fdfcbd24d6882b9edebb750a16148542
junhaobearxiong <jxiong3@jhu.edu>
2019-05-22 17:30:01 +0200

    remove celegans sh

--- run_celegans.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PYTHONPATH=/Users/jxiong/Documents/Projects/mgcpy:$(pwd)
-python bin/celegans_chem_gap.py 500 1 0
-python bin/celegans_chem_gap.py 500 1 1
-python bin/celegans_chem_gap.py 500 1 2
