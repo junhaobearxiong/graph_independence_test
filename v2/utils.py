ccde50f07a8cfaf4dde0ab491ac4f05c5d80ff0b
junhaobearxiong <jxiong3@jhu.edu>
2020-11-28 16:01:23 +0800

    add pvalue based on block permutation

--- v2/utils.py
+++ v2/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 from scipy.stats import pearsonr
+from core import block_permutation, gcorr
 
 
 def pearson_graph(G1, G2):
@@ -43,3 +44,22 @@ def power_estimation(test_stats_null, test_stats_alt, alpha):
     c2 = np.sort(test_stats_null)[math.ceil((1 - alpha / 2) * nmc)]
     power = np.where((test_stats_alt > c2) | (test_stats_alt < c1))[0].size / nmc
     return power
+
+
+def permutation_pvalue(G1, G2, Z, num_perm):
+    """
+    Estimate p-value via a block permutation test
+    """
+    obs_test_stat = gcorr(G1, G2, Z)
+    null_test_stats = np.zeros(num_perm)
+    for i in range(num_perm):
+        G2_perm = block_permutation(G2, Z)
+        null_test_stats[i] = gcorr(G1, G2_perm, Z)
+    num_extreme = np.where(null_test_stats >= obs_test_stat)[0].size
+    if num_extreme < num_perm / 2:
+        # P(T > t | H0) is smaller 
+        return 2 * num_extreme / num_perm
+    else:
+        # P(T < t | H0) is smaller
+        return 2 * (num_perm - num_extreme) / num_perm
+
